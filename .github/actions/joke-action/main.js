38a67f48f2407efd60d9c05bdd6fba605d6d0e07
timphamaus <154489049+timphamaus@users.noreply.github.com>
2024-01-15 06:45:40 +0000

    edit main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -2,7 +2,7 @@ const getJoke = require('./joke');
 const core = require('@actions/core');
 
 async function run() {
-    const joke = getJoke();
+    const joke = await getJoke();
     console.log(joke);
     core.setOutput('joke-output', joke);
 }
